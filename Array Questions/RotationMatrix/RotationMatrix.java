3f5dcc1df5d2161ff64b10b7757a1cd06631677b
Deepak Atariya <deepakkumaratariya@gmail.com>
2020-07-19 13:06:58 +0530

    solved inplace and outplace both

--- Array Questions/RotationMatrix/RotationMatrix.java
+++ Array Questions/RotationMatrix/RotationMatrix.java
@@ -4,7 +4,7 @@ public class RotationMatrix {
 
     // inplace solution is required
 
-    // brute force method
+    // outplace sollution
     public static int[][] rotationMatrixOutplace(int matrix[][]) {
         int temp[][] = new int[matrix.length][matrix.length];
         for (int i = 0; i < matrix.length; i++) {
@@ -17,6 +17,22 @@ public class RotationMatrix {
         return temp;
     }
 
+    // inplace solution
+    public static int[][] rotationMatrixInplace(int matrix[][]) {
+
+        for (int i = 0; i < matrix.length/2; i++) {
+            for (int j = 0; j < matrix.length-i-1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length-1-j][i];
+                matrix[matrix.length-1-j][i] = matrix[matrix.length-1-i][matrix.length-1-j];
+                matrix[matrix.length-1-i][matrix.length-1-j] = matrix[j][matrix.length-1-i]; 
+                matrix[j][matrix.length-1-i] = temp;
+            }
+        }
+
+        return matrix;
+    }
+
 
 
     public static void main(String[] args) {
@@ -25,12 +41,12 @@ public class RotationMatrix {
             {4,5,6},
             {7,8,9},
         };
-        int results[][] = rotationMatrixOutplace(matrix);
-
+        
         System.out.println("input :");
         for (int i=0; i<matrix.length; i++) {
             System.out.println(Arrays.toString(matrix[i]));
         }
+        int results[][] = rotationMatrixInplace(matrix);
         
         System.out.println("output :");
         for (int i=0; i<results.length; i++) {
