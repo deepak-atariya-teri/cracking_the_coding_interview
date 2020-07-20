6c4c71914e7c042c197bdfa9463785baa8b970e2
Deepak Atariya <deepakkumaratariya@gmail.com>
2020-07-21 00:03:20 +0530

    problem is to find all subsets by recursion

--- knapsack01/Knapsack01.java
+++ knapsack01/Knapsack01.java
@@ -1,7 +1,19 @@
 
 public class Knapsack01 {
 
+    public static int[] subset(int weights[], int u, int v) {
+
+        return new int[]{1,2,3};
+    }
+    
+
     public static void knapsack01(int weights[], int values[], int bag) {
+
+        // 1. find all the subset
+        // 2. test with all subset and build an array 
+        // 3. find a maximum value
+
+        // int weights1[] = subset(weights, 0, 1);
         
     }
 
