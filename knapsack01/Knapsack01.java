ac80d62d8932b30554cf6cc6c63a4035220c2da6
Deepak Atariya <deepakkumaratariya@gmail.com>
2020-07-20 23:44:15 +0530

    knapsack recursion sucks

---
+++ knapsack01/Knapsack01.java
@@ -0,0 +1,16 @@
+
+public class Knapsack01 {
+
+    public static void knapsack01(int weights[], int values[], int bag) {
+        
+    }
+
+    public static void main(String[] args) {
+        int weights[] = {10,20,30};
+        int values[] = {60,100,120};
+        int bag = 50;
+
+
+        knapsack01(weights, values, bag);
+    }
+}
